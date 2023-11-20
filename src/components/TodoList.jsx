329e3cc80c7543288338ebfb266cbdcbdfa7ae9e
CorinaTalledo <corina.talledo@gmail.com>
2023-11-19 23:10:53 -0300

    styles to task container (TodoList)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,8 +3,7 @@ import TodoTask from "./TodoTask.jsx";
 
 export default function TodoList() {
     return (
-        <Box border='3px solid' borderColor='teal.300' rounded='md'>
-            TodoList
+        <Box border='3px solid' borderColor='teal.300' rounded='md' w="40%" minH="50vh" align="center" boxShadow='2xl' p='8' borderStyle='dashed' mx="auto" mt="4%">
             <TodoTask />
         </Box>
     )
