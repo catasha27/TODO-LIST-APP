8bd9560ccf54a3ef6de1927f7609a4b3e25b0011
CorinaTalledo <corina.talledo@gmail.com>
2023-11-20 11:09:28 -0300

    UnorderedList component added (father of tasks)

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,13 @@
 import { Box } from '@chakra-ui/react'
 import TodoTask from "./TodoTask.jsx";
+import { UnorderedList } from '@chakra-ui/react'
 
 export default function TodoList() {
     return (
-        <Box border='3px solid' borderColor='teal.300' rounded='md' w="40%" minH="50vh" align="center" boxShadow='2xl' p='8' borderStyle='dashed' mx="auto" mt="4%">
-            <TodoTask />
+        <Box border='3px solid' borderColor='teal.300' rounded='md' w="40%" minH="50vh" boxShadow='2xl' p='10' borderStyle='dashed' mx="auto" mt="4%">
+            <UnorderedList >
+                <TodoTask />
+            </UnorderedList>
         </Box>
     )
 }
