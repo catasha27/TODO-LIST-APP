10ebe44c921c3df664c7cac93a91775e1e74268e
CorinaTalledo <corina.talledo@gmail.com>
2023-11-21 14:48:10 -0300

    Fix styles of app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Header from "./components/Header.jsx";
 import Form from "./components/Form.jsx";
 import TodoList from "./components/TodoList.jsx";
 import Footer from "./components/Footer.jsx";
-import { Flex } from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
 
 function App() {
   return (
-    <Flex minH='100vh' direction="column" bgGradient='linear(to-b, #1100B7, #040041)' color='white'>
+    <Flex minH='100vh' direction="column" bgGradient='linear(to-b, #1100B7, #040041)' color='white' pb="5">
       {/* <Box border='3px solid' borderColor='pink.600' rounded='md'> */}
         <Header />
         <Form />
+        <Spacer />
         <TodoList />
+        <Spacer />
         <Footer />
       {/* </Box> */}
     </Flex>
