12fcaae52df979599b9c61ad4bbd25afdbd0f2d4
CorinaTalledo <corina.talledo@gmail.com>
2023-11-20 23:01:30 -0300

    Box component deleted. Styles are on unordered list component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,10 @@
-import { Box } from '@chakra-ui/react'
 import TodoTask from "./TodoTask.jsx";
 import { UnorderedList } from '@chakra-ui/react'
 
 export default function TodoList() {
     return (
-        <Box border='3px solid' borderColor='teal.300' rounded='md' w="40%" minH="50vh" boxShadow='2xl' p='10' borderStyle='dashed' mx="auto" mt="4%">
-            <UnorderedList >
-                <TodoTask />
-            </UnorderedList>
-        </Box>
+        <UnorderedList border='3px solid' borderColor='teal.300' rounded='md' w="600px" minH="30vh" boxShadow='2xl' p='10' borderStyle='dashed' mx="auto" my="4%">
+            <TodoTask />
+        </UnorderedList>
     )
 }
