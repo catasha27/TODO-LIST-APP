16ca3ad88f91f4a913039abda59fbaced79fe45a
CorinaTalledo <corina.talledo@gmail.com>
2023-11-20 23:02:33 -0300

    Add check icon component

--- src/components/IconBtn.jsx
+++ src/components/IconBtn.jsx
@@ -1,9 +1,11 @@
 import { Box } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/react'
+import { CheckIcon } from '@chakra-ui/icons'
 
 export default function IconBtn() {
     return (
         <Box border='3px solid' borderColor='orange.500' rounded='md'>
-            IconBtn
+            <IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Done' fontSize='15px' icon={<CheckIcon />} />
         </Box>
     )
 }
