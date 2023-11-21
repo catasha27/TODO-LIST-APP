90663518d5db477dba49f69cdea041d8e67b2a8e
CorinaTalledo <corina.talledo@gmail.com>
2023-11-21 13:32:49 -0300

    Fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Heading } from "@chakra-ui/react";
 export default function Header() {
   return (
     <Heading as="h1" size="4xl" align="center" justify="center">
-      To do List
+      Todo List
     </Heading>
   );
 }
