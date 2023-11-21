51c18e0af7a712e1d39d52f1dde81bc6a17d403b
CorinaTalledo <corina.talledo@gmail.com>
2023-11-21 13:54:48 -0300

    fix layout todo task component

--- src/components/TodoTask.jsx
+++ src/components/TodoTask.jsx
@@ -1,19 +1,16 @@
 import IconBtn from "./IconBtn.jsx";
-import { ListItem } from "@chakra-ui/react";
-import { Flex, Spacer } from "@chakra-ui/react";
-import { ButtonGroup } from "@chakra-ui/react";
+import { ListItem, Spacer, ButtonGroup } from "@chakra-ui/react";
+
 
 export default function TodoTask() {
     return (
-        <Flex>
-            <ListItem>Lorem ipsum dolor sit</ListItem>
-
+        <ListItem>Lorem ipsum dolor sit
             <Spacer />
 
             <ButtonGroup>
                 <IconBtn isTrash={false} />
                 <IconBtn isTrash={true} />
             </ButtonGroup>
-        </Flex>
+        </ListItem>
     );
 }
