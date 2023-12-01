59400b934ea023858c75794bd6660001f25cb7b9
Sharime Cahuas <sharimecahuas@gmail.com>
2023-12-01 16:17:00 -0300

    add Form component validations message

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,37 +1,53 @@
-import { FormControl, FormLabel, Input, Flex, IconButton } from "@chakra-ui/react";
+import { FormControl, FormLabel, FormErrorMessage, Input, Flex, IconButton } from "@chakra-ui/react";
 import { AddIcon } from '@chakra-ui/icons'
 import { useState } from "react";
 
 
-export default function AddTask({ onSubmit }) {
+export default function AddTask({ onSubmit, tasks }) {
 
   const [description, setDescription] = useState('');
+  const [error, setError] = useState('')
+
+  const validateForm = () => {
+    if (description.trim() === "") {
+      return "Please complete this field"
+    }
+    const isRepeated = tasks.find((task) => task.description.toLowerCase() === description.toLowerCase())
+    if (isRepeated) {
+      return "This task already exists, please double check the existing tasks"
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (description.trim() !== "") {
+    const error = validateForm()
+    if (!error) {
       const task = {
         id: crypto.randomUUID(),
         description: description,
         completed: false
       }
-
+      setError('')
       onSubmit(task);
-      setDescription("");
+      setDescription('');
+
+    } else {
+      setError(error)
     }
-
-
   };
 
   return (
-    <Flex as='form' w='100%' mx='auto' alignItems='end' onSubmit={handleSubmit} >
-      
-      <FormControl direction='column' w='100%' mr="5">
+    <Flex as='form' noValidate w='100%' mx='auto' alignItems='end' justifyContent='center' onSubmit={handleSubmit} >
+
+      <FormControl direction='column' w='100%' isRequired isInvalid={error}>
         <FormLabel>Task</FormLabel>
-        <Input type="text" border='2px' w='100%' maxLength="50" placeholder="Add a task to the list" value={description} onChange={(e) => setDescription(e.target.value)} />
+        <Flex justifyContent='space-between' gap='4' >
+          <Input type="text" border='2px' maxLength="50" placeholder="Add a task to the list" value={description} onChange={(e) => setDescription(e.target.value)} />
+          <IconButton aria-label='Add task' type='submit' icon={<AddIcon />} />
+        </Flex>
+        <FormErrorMessage>{error}</FormErrorMessage>
       </FormControl>
 
-      <IconButton aria-label='Add task' type='submit' icon={<AddIcon />} />
 
     </Flex>
   );
