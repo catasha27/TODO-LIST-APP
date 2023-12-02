7240e1ce2b583597c52600a0f08196374df719a8
CorinaTalledo <corina.talledo@gmail.com>
2023-12-02 16:21:20 -0300

    Add OnClick as a prop and use it on IconButton

--- src/components/IconBtn.jsx
+++ src/components/IconBtn.jsx
@@ -1,8 +1,10 @@
 import { IconButton } from '@chakra-ui/react'
 import { CheckIcon, DeleteIcon } from '@chakra-ui/icons'
 
-export default function IconBtn({ isTrash }) {
+export default function IconBtn({ isTrash, onClick }) {
+
     return (
-        <IconButton isRound={true} variant='solid' colorScheme='teal' aria-label='Done' fontSize='15px' icon={ isTrash ? <DeleteIcon /> : <CheckIcon />} />
+        <IconButton isRound={true} variant='solid' colorScheme='teal' aria-label={isTrash ? 'Delete' : 'Done'} fontSize='15px' icon={isTrash ? <DeleteIcon /> : <CheckIcon />}
+        onClick={onClick} />
     )
 }
