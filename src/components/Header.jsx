dbc28f2c123b2d6093d26bbe9414d38ed6cb7020
Sharime Cahuas <sharimecahuas@gmail.com>
2023-12-06 18:01:39 -0300

    add images and styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-import { Heading } from "@chakra-ui/react";
+import { Center, Heading, Image } from "@chakra-ui/react";
+import board from '../assets/clipboard-list-solid-blue.svg'
+import clip from '../assets/paperclip-solid-blue.svg'
 
 export default function Header() {
   return (
-    <Heading as="h1" size="4xl" align="center" justify="center">
-      Todo List
-    </Heading>
+    <Center alignItems='center'>
+      <Image boxSize='70px' mr='20px' src={board} />
+      <Heading as='h1'
+        bgGradient='radial(gray.400, red.500 50%)'
+        bgClip='text'
+        fontSize='7xl'
+        fontWeight='bold'
+      >
+        Todo List
+      </Heading>
+      <Image boxSize='55px' ml='20px' src={clip} />
+    </Center>
   );
 }
