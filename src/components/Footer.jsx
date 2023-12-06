f8c202bd556f39bfa544a8f9191115dc160df7fc
Sharime Cahuas <sharimecahuas@gmail.com>
2023-12-06 16:33:44 -0300

    add styles to footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Text } from '@chakra-ui/react'
 export default function Footer() {
     return (
         <Text fontSize='sm' mx="auto">
-            Made with ♥ by Sharime Cahuas and Corina Talledo
+            Made with ♥ by <Text as='i' color='teal.600' fontSize='lg' >Sharime Cahuas</Text> and <Text as='i' color='red.500' fontSize='lg'>Corina Talledo</Text>
         </Text>
     )
 }
