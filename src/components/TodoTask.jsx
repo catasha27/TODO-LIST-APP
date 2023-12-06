8bffe95e54c4b4f4561c00d72e60122ddbb569e9
Sharime Cahuas <sharimecahuas@gmail.com>
2023-12-06 15:47:01 -0300

    fix list items layout

--- src/components/TodoTask.jsx
+++ src/components/TodoTask.jsx
@@ -1,17 +1,18 @@
 import IconBtn from "./IconBtn.jsx";
 import { StarIcon } from '@chakra-ui/icons'
-import { Box, ListItem, ListIcon, Spacer, ButtonGroup } from "@chakra-ui/react";
+import { Box, ListItem, ListIcon, Text, Spacer, ButtonGroup } from "@chakra-ui/react";
 
 
 export default function TodoTask({ description, id, completed, deleteTask, toggle }) {
     return (
-        <Box as={ListItem} justifyContent='space-between' fontSize='lg'
+        <Box as={ListItem} display='flex' alignItems='center' fontSize='lg' mb='10px'
         style={{ textDecoration: completed ? 'line-through' : 'none', color: completed ? 'gray' : 'white' }}>
-            <ListIcon as={StarIcon} color={completed ? 'gray' : 'blue.600'} />{description}
+            <ListIcon as={StarIcon} color={completed ? 'gray' : 'blue.600'} />
+            {description}
 
-            {/* <Spacer /> */}
+            <Spacer />
 
-            <ButtonGroup>
+            <ButtonGroup gap='10px'>
                 <IconBtn isTrash={false} onClick={() => toggle(id)} />
                 <IconBtn isTrash={true} onClick={() => deleteTask(id)} />
             </ButtonGroup>
